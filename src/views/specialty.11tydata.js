3c83cb4b6095a8d436be5d5e7409fcb41fdd01b8
Igor Korovchenko <igsekor@gmail.com>
2023-05-08 12:38:52 +0300

    Добавляет шаблон для уровня направления подготовки

---
+++ src/views/specialty.11tydata.js
@@ -0,0 +1,30 @@
+module.exports = {
+  layout: 'base.njk',
+
+  pagination: {
+    data: 'collections.qasBySpecialty',
+    size: 1,
+    alias: 'qas',
+  },
+
+  permalink: '/{{qas}}/index.xml',
+
+  eleventyComputed: {
+    qaList: function (data) {
+      const { collections, qas } = data
+      const parts = qas.split('/')
+      const qaAsTree = collections.qasAsTree
+      const result = []
+      if (Object.keys(qaAsTree).includes(parts[0])) {
+        const specialty = qaAsTree[parts[0]]
+        Object.keys(specialty).forEach((competence) => {
+          Object.keys(qaAsTree[parts[0]][competence]).forEach((discipline) => {
+            result.push(...qaAsTree[parts[0]][competence][discipline])
+          })
+        })
+      }
+      console.log(result)
+      return result
+    },
+  },
+}
